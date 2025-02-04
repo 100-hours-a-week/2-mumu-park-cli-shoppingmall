2a9778f3c798f8229af7a167fe94d4168f086762
박성춘 <songchoon10@naver.com>
2025-02-05 00:27:21 +0900

    chore: 매직넘버 적용하여 가독성 향상

--- src/validator/PointValidator.java
+++ src/validator/PointValidator.java
@@ -4,12 +4,13 @@ import constant.exception.custom.NoSuchPointException;
 import constant.exception.custom.ValidatorClassException;
 
 public class PointValidator {
+    private static final int ZERO_POINT = 0;
     private PointValidator() throws ValidatorClassException {
         throw new ValidatorClassException();
     }
 
     public static void checkExistPointToUse(int totalPoint) {
-        if (totalPoint == 0) {
+        if (totalPoint == ZERO_POINT) {
             throw new NoSuchPointException();
         }
     }
